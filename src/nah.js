279052261a25029b3e3f5e6dc541f716dcb7f922
kttFox <kttFoxOnFire@gmail.com>
2025-04-01 15:29:24 +0900

    Update nah.js

--- src/nah.js
+++ src/nah.js
@@ -69,7 +69,7 @@ async function addNahBtns(videoBoxSelector) {
             "M12 3c-4.96 0-9 4.04-9 9s4.04 9 9 9 9-4.04 9-9-4.04-9-9-9m0-1c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zm7 11H5v-2h14v2z"
         ),
         cssClass: "btn-bottom",
-        textContent: "❌",
+        textContent: "✕",
     };
     const btnsToAdd = [];
     const shouldHideNahButton = await getFromStorage("shouldHideNahButton");
